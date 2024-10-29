16a1e1f1be5365f848f13672294a68c0f109f3d3
brucewaynepioneer <158065590+brucewaynepioneer@users.noreply.github.com>
2024-10-29 12:34:12 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -641,7 +641,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
             if msg.empty is not None:
                 await client.delete_messages(chat_id=sender, message_ids=edit_id)
                 return None            
-            if msg.media and msg.media == MessageMediaType.WEB_PAGE_PREVIEW:
+            if msg.media and msg.media == MessageMediaType.WEB_PAGE:
                 a = b = True
                 edit = await client.edit_message_text(sender, edit_id, "Cloning.")
                 if '--'  in msg.text.html or '**' in msg.text.html or '__' in msg.text.html or '~~' in msg.text.html or '||' in msg.text.html or '```' in msg.text.html or '`' in msg.text.html:
